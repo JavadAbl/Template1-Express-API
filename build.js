11d15245a4104751ad61c10774de12511c6ebe0a
Javadabl <com.javadabl@gmail.com>
2025-09-19 23:24:48 +0330

    C

---
+++ build.js
@@ -0,0 +1,26 @@
+// build.js
+
+import { build } from "esbuild";
+
+build({
+  entryPoints: ["dist/index.js"],
+  bundle: true,
+  platform: "node",
+  target: ["node24"],
+  outfile: "dist/bundle.js",
+  format: "cjs", // ES module output
+  external: ["fs", "path"],
+
+  /*   loader: { ".js": "js" }, // treat .js as ESM
+  define: { require: "undefined" }, // optional safety net */
+});
+
+/* build({
+  entryPoints: ["dist/index.js"],
+  bundle: true,
+  platform: "node", // Node.js runtime
+  target: ["node24"], // Adjust to your Node version
+  outfile: "dist/bundle.js",
+  external: ["fs", "path"], // Keep built‑ins external
+  minify: true,
+}).catch(() => process.exit(1)); */
